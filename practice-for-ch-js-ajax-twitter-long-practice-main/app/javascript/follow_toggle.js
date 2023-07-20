1224e583756a9c1c53245bdec34175ee1def806c
Elliot Kuok <elliotckuok@gmail.com>
2023-07-19 17:24:35 -0700

    Adding backend toggle button

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -3,23 +3,65 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
+    this.toggleButton = toggleButton;
+    this.handleClick = this.handleClick.bind(this);
+    this.toggleButton.addEventListener("click", this.handleClick);
+    this.followState = "following";
+    this.render();
+    broadcast(this.followState);
+    API.follow(this.followState);
   }
 
   async handleClick(event) {
     // Your code here
+    event.preventDefault();
+    if (this.followState === "following") {
+      await this.unfollow();
+    } else {
+      await this.follow();
+    }
+    this.followState = this.followState === "following"? "unfollowing" : "following";
+    
   }
 
   async follow() {
-    // Your code here
+    this.toggleButton.disabled = true;
+    this.toggleButton.innerText = "Following..."
+    try {
+      this.followState = "following";
+      this.render();
+    } catch (error) {
+      console.error("Error following user:", error)
+    }
+
+    this.toggleButton.disabled = false;
   }
 
   async unfollow() {
-    // Your code here
+    this.toggleButton.disabled = true;
+    this.toggleButton.innerText = "Unfollowing..."
+    try {
+      this.followState = "unfollowed";
+      this.render();
+    } catch (error) {
+      console.error("Error following user:", error)
+    }
+
+    this.toggleButton.disabled = false;
   }
 
   render() {
     switch (this.followState) {
-      // Your code here
+      case "following":
+        this.toggleButton.innerText = "Unfollow";
+        break;
+      case "unfollowed":
+        this.toggleButton.innerText = "Follow";
+        break;
+      default:
+        this.toggleButton.innerText = "Loading...";
+        this.toggleButton.disabled = true;
+        break;
     }
   }
 
@@ -31,4 +73,26 @@ export default class FollowToggle {
     this.toggleButton.dataset.followState = newState;
     this.render();
   }
+}
+
+const FollowToggleButton = document.getElementsByClassName("follow-toggle");
+
+async function handleFollowUnfollow() {
+
+  const isFollowing = await followState();
+  const method = isFollowing ? "DELETE" : "POST";
+
+  try {
+    const res = await fetch('/users/${userId}/follow', {
+      method: method,
+      // body: JSON.stringify({gif}), //{gif: gif}
+      headers:{'Content-Type': 'application/json', 'Accept': 'application/json'}
+    });
+    if (res.ok) {
+      isFollowing ? (followToggleButton.innerText = "Follow") : (followToggleButton.innerText = "Unfollow")
+    }
+  } catch (error) {
+    console.error("Error occurred during follow/unfollow:", error);
+  }
+  
 }
